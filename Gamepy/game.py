6a8a300462859751f6cf8060aaac7222ddeb4c5e
vitor <vitor.scastro2013@hotmail.com>
2023-03-15 18:01:31 -0300

    Novas atualizações

--- Gamepy/game.py
+++ Gamepy/game.py
@@ -1,3 +1,18 @@
+"""
+Projeto 1 - Game
+
+Devemos desenvolver uma aplicação onde ao ser inicializada solicite ao usuário escolher o nível de
+dificuldade do jogo e após isso gere e apresenta, de forma aleatória, um cálculo para que possamos 
+informar o resultado.
+
+Iremos limitar as opções em somar, subtrair e multiplicar.
+
+Se o usuário acertar a resposta, somará 1 ponto ao seu score.
+
+Acertando ou errando, ele poderá ou não continuar o jogo.
+
+"""
+
 from models.calcular import Calcular
 
 def main() -> None:
